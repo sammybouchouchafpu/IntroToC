b256c914cdaeba9e541de0e2d3b9197c2a2710e4
Sammy Bouchoucha <sbouchoucha9043@floridapoly.edu>
2025-01-21 16:15:36 +0000

    Changed variables to float to allow support for decimal values

--- 05_years_to_minutes/years_to_minutes.c
+++ 05_years_to_minutes/years_to_minutes.c
@@ -5,19 +5,19 @@
 #include <stdio.h>
 
 int main() {
-    int year_amount = 0;
+    float year_amount = 0;
     // Prompt
     puts("Enter your age in years:");
 
     // Get input
-    scanf("%d", &year_amount);
+    scanf("%f", &year_amount);
 
     // Result
-    int minute_amount = year_amount * 525600;
-    if (year_amount > 1 || year_amount == 0) {
-        printf("\"%d\" years equates to \"%d\" minutes.\n", year_amount, minute_amount);
-    }   else if (year_amount == 1) {
-        printf("\"%d\" year equates to \"%d\" minutes.\n", year_amount, minute_amount);
+    float minute_amount = year_amount * 525600;
+    if (year_amount == 1) {
+        printf("\"%f\" year equates to \"%f\" minutes.\n", year_amount, minute_amount);
+    }   else if (year_amount > 0 || year_amount == 0) {
+        printf("\"%f\" years equates to \"%f\" minutes.\n", year_amount, minute_amount);
     }   else {
         printf("Age cannot be negative!\n");
     }
